dc76e2c5cca55508f72af63c85a8425b83c140be
Charles <mo.charles2019@gmail.com>
2024-03-20 09:17:25 -0400

    add dealer detail endpoints

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -88,18 +88,34 @@ def get_cars(request):
             "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id):
+    if (dealer_id):
+        endpoint = f"/fetchReviews/dealer/{dealer_id}"
+        reviews = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer": reviews})
+    return JsonResponse({"status":400,"message": "Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if (dealer_id):
+        endpoint = f"/fetchDealer/{dealer_id}"
+        details = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"dealer": details})
+    return JsonResponse({"status":400,"message": "Bad Request"})
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
